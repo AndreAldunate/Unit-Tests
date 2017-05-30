994aa6bd64a54200af2deb9340bb4e828cdc8140
AndreAldunate <andre_aldunate@hotmail.com>
2017-05-30 10:08:05 -0400

    Add files via upload

--- Unit Tests/src/main/java/Main.java
+++ Unit Tests/src/main/java/Main.java
@@ -7,7 +7,7 @@ import java.util.Date;
 public class Main {
 
 	public static void main(String[] args) {
-	System.out.println(primo(21));
+	System.out.println(edad("04/12/1996"));
 	}
 
 	public static Persona aplica(Persona a, Persona b, String p) {
@@ -66,5 +66,9 @@ class Persona {
 		nombre = n;
 		profesion = p;
 		edad = e;
+	}
+	@Override
+	public String toString(){
+		return nombre;
 	}
 }
